89d699082313d68afb62685511794d46612af16e
Adrian-Ver <avsfederizo@gmail.com>
2017-12-03 02:46:34 +0800

    My Assignment 2
    Caching the inverse of a square matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The following functions cooperatively caches the inverse of a matrix
 
-## Write a short comment describing this function
+# The following function creates a list which sets up the matrix to be 
+    # inverted and cached
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL 
+    set_mtx <- function(mtx1){
+        x <<- mtx1 
+        inv <- NULL 
+    }
+    get_mtx <- function(){x} 
+    set_inv <- function(minv){inv <<- minv}
+    get_inv <- function(){inv}
+    list(set_mtx = set_mtx,
+         get_mtx = get_mtx,
+         set_inv = set_inv,
+         get_inv = get_inv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$get_inv()
+    if(!is.null(inv)) {
+        message("Getting cached inverse...")
+        return(inv)
+    }
+    data <- x$get_mtx()
+    inv <- solve(data)
+    x$set_inv(inv)
+    inv
 }
